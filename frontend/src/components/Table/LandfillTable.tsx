f757e181f88d5fe97ab49c9b721a976656b4d321
ahmedfahad04 <bsse1204@gmail.com>
2024-03-28 14:30:39 +0600

    added basic landfill table

---
+++ frontend/src/components/Table/LandfillTable.tsx
@@ -0,0 +1,137 @@
+import { ILandfill } from "@/models/Landfill";
+import { dummyLandfill } from "@/utils/DummyData";
+// import { dummyLandfill } from "@/utils/DummyData"; // Importing static data
+import { EditIcon, Trash2Icon } from "lucide-react";
+import {
+  MRT_ActionMenuItem,
+  MRT_ColumnDef,
+  MaterialReactTable,
+} from "material-react-table";
+import { useEffect, useMemo, useState } from "react";
+// import EditLandfillModal from "../Modals/Landfill/EditLandfillModal";
+// import ViewLandfillModal from "../Modals/Landfill/ViewLandfillModal";
+
+const LandfillTable = () => {
+  const [showEditLandfillModal, setShowEditLandfillModal] =
+    useState<boolean>(false);
+  const [showLandfillModal, setShowLandfillModal] = useState<boolean>(false);
+  const [LandfillData, setLandfillData] = useState<ILandfill>();
+  const [data, setData] = useState<ILandfill[]>([]);
+
+  const handleRowDelete = (index: string, closeWindow: () => void) => {
+    if (window.confirm("Are you sure?")) {
+      // Create a copy of the current data
+      const newData = [...data];
+      // Remove the entry at the specified index
+      //! api call to delete entry
+      newData.splice(parseInt(index), 1);
+      // Update the data state
+      setData(newData);
+      closeWindow();
+    }
+  };
+
+  useEffect(() => {
+    //! fetch using useSWR
+    setData(dummyLandfill); // Set static data from dummyLandfill
+  }, []); // Empty dependency array to run only once when component mounts
+
+  const columns = useMemo<MRT_ColumnDef<ILandfill>[]>(
+    () => [
+      {
+        accessorKey: "landfillName",
+        header: "Landfill NAME",
+        size: 180,
+      },
+      {
+        accessorKey: "openingTime",
+        header: "OPENING TIME",
+        size: 150,
+      },
+      {
+        accessorKey: "endingTime",
+        header: "ENDING TIME",
+        size: 150,
+      },
+      {
+        accessorKey: "capacity",
+        header: "CAPACITY (TON)",
+        size: 150,
+      },
+      {
+        accessorKey: "latitude",
+        header: "LATITUDE",
+        size: 150,
+      },
+      {
+        accessorKey: "longitude",
+        header: "LONGITUDE",
+        size: 150,
+      },
+    ],
+    []
+  );
+
+  return (
+    <div>
+      <p className="mb-5">
+        <span className="font-bold">{data.length}</span> in total
+      </p>
+      <MaterialReactTable
+        columns={columns}
+        data={data}
+        enableRowActions
+        enableStickyHeader
+        muiTableContainerProps={{ sx: { maxHeight: "500px" } }}
+        renderRowActionMenuItems={({ closeMenu, row, table }) => [
+          <MRT_ActionMenuItem
+            icon={<EditIcon className="text-blue-500" />}
+            key="edit"
+            label="Edit"
+            onClick={() => {
+              setLandfillData(row.original);
+              setShowEditLandfillModal(true);
+              closeMenu();
+            }}
+            table={table}
+            className="bg-blue-200"
+          />,
+          <MRT_ActionMenuItem
+            icon={<Trash2Icon className="text-red-500" />}
+            key="delete"
+            label="Delete"
+            onClick={() => {
+              handleRowDelete(row.id, closeMenu);
+            }}
+            table={table}
+          />,
+        ]}
+        muiTableBodyRowProps={({ row }) => ({
+          onClick: () => {
+            setLandfillData(row.original);
+            setShowLandfillModal(true);
+          },
+          sx: { cursor: "pointer" },
+        })}
+      />
+
+      {/* {showEditLandfillModal && (
+        <EditLandfillModal
+          isOpen={showEditLandfillModal}
+          onClose={() => setShowEditLandfillModal(false)}
+          LandfillData={LandfillData}
+        />
+      )}
+
+      {showLandfillModal && (
+        <ViewLandfillModal
+          isOpen={showLandfillModal}
+          onClose={() => setShowLandfillModal(false)}
+          LandfillData={LandfillData}
+        />
+      )} */}
+    </div>
+  );
+};
+
+export default LandfillTable;
--- frontend/src/pages/Landfill.tsx
+++ frontend/src/pages/Landfill.tsx
@@ -1,7 +1,38 @@
+import LandfillTable from "@/components/Table/LandfillTable";
 import Layout from "@/layout/Layout";
+import { useState } from "react";
 
 const Landfill = () => {
-  return <Layout>Landfill</Layout>;
+  const [showCreateLandfillModal, setShowCreateLandfillModal] =
+    useState<boolean>(false);
+
+  return (
+    <Layout>
+      <div className="h-screen flex flex-col w-full p-5 px-10">
+        {/* top header */}
+        <div className="h-20 flex flex-row justify-between items-center ">
+          <p className="font-bold px-2 text-3xl break-words">Landfill Sites</p>
+          <button
+            className="h-8 mr-8 text-center flex items-center p-3 text-sm font-normal bg-[#14923EFF] text-white rounded-md hover:bg-[#177737]"
+            onClick={() => setShowCreateLandfillModal(!showCreateLandfillModal)}
+          >
+            + New Landfill
+          </button>
+        </div>
+
+        {/* {showCreateLandfillModal && (
+          <AddLandfillModal
+            isOpen={showCreateLandfillModal}
+            onClose={() => setShowCreateLandfillModal(false)}
+          />
+        )} */}
+
+        <div className="flex-1 mt-10">
+          <LandfillTable />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Landfill;
