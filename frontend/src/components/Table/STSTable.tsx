bac8ae33d66633aaf3589d122109fbf6cde89d4a
ahmedfahad04 <bsse1204@gmail.com>
2024-03-28 13:54:03 +0600

    added Delete entry of STS Table

--- frontend/src/components/Table/STSTable.tsx
+++ frontend/src/components/Table/STSTable.tsx
@@ -1,46 +1,63 @@
 import { ISTS } from "@/models/STS";
-import { dummySTS } from "@/utils/DummyData";
+import { dummySTS } from "@/utils/DummyData"; // Importing static data
 import { EditIcon, Trash2Icon } from "lucide-react";
 import {
   MRT_ActionMenuItem,
   MRT_ColumnDef,
   MaterialReactTable,
 } from "material-react-table";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import EditSTSModal from "../Modals/STS/EditSTSModal";
 import ViewSTSModal from "../Modals/STS/ViewSTSModal";
 
-const data = dummySTS;
-
 const STSTable = () => {
-  const [showEditSTSModal, setShowEditSTSModal] = useState<boolean>();
-  const [showSTSModal, setShowSTSModal] = useState<boolean>();
+  const [showEditSTSModal, setShowEditSTSModal] = useState<boolean>(false);
+  const [showSTSModal, setShowSTSModal] = useState<boolean>(false);
   const [STSData, setSTSData] = useState<ISTS>();
+  const [data, setData] = useState<ISTS[]>([]);
+
+  const handleRowDelete = (index: number, closeWindow: () => {}) => {
+    if (window.confirm("Are you sure?")) {
+      // Create a copy of the current data
+      const newData = [...data];
+      // Remove the entry at the specified index
+      //! api call to delete entry
+      newData.splice(index, 1);
+      // Update the data state
+      setData(newData);
+      closeWindow();
+    }
+  };
+
+  useEffect(() => {
+    //! fetch using useSWR
+    setData(dummySTS); // Set static data from dummySTS
+  }, []); // Empty dependency array to run only once when component mounts
 
   const columns = useMemo<MRT_ColumnDef<ISTS>[]>(
     () => [
       {
-        accessorKey: "STSName", //access nested data with dot notation
+        accessorKey: "STSName",
         header: "STS NAME",
         size: 180,
       },
       {
-        accessorKey: "wardNumber", //access nested data with dot notation
+        accessorKey: "wardNumber",
         header: "WARD NUMBER",
         size: 150,
       },
       {
-        accessorKey: "capacity", //access nested data with dot notation
+        accessorKey: "capacity",
         header: "CAPACITY (TON)",
         size: 150,
       },
       {
-        accessorKey: "latitude", //access nested data with dot notation
+        accessorKey: "latitude",
         header: "LATITUDE",
         size: 150,
       },
       {
-        accessorKey: "longitude", //access nested data with dot notation
+        accessorKey: "longitude",
         header: "LONGITUDE",
         size: 150,
       },
@@ -57,22 +74,15 @@ const STSTable = () => {
         columns={columns}
         data={data}
         enableRowActions
-        // positionActionsColumn="last"
         enableStickyHeader
         muiTableContainerProps={{ sx: { maxHeight: "500px" } }}
         renderRowActionMenuItems={({ closeMenu, row, table }) => [
-          <MRT_ActionMenuItem //or just use a normal MUI MenuItem component
+          <MRT_ActionMenuItem
             icon={<EditIcon className="text-blue-500" />}
             key="edit"
             label="Edit"
             onClick={() => {
-              setSTSData({
-                STSName: row.original.STSName,
-                wardNumber: row.original.wardNumber,
-                capacity: row.original.capacity,
-                longitude: row.original.longitude,
-                latitude: row.original.latitude,
-              });
+              setSTSData(row.original);
               setShowEditSTSModal(true);
               closeMenu();
             }}
@@ -84,21 +94,14 @@ const STSTable = () => {
             key="delete"
             label="Delete"
             onClick={() => {
-              alert("Delete");
-              closeMenu();
+              handleRowDelete(row.id, closeMenu);
             }}
             table={table}
           />,
         ]}
         muiTableBodyRowProps={({ row }) => ({
           onClick: () => {
-            setSTSData({
-              STSName: row.original.STSName,
-              wardNumber: row.original.wardNumber,
-              capacity: row.original.capacity,
-              longitude: row.original.longitude,
-              latitude: row.original.latitude,
-            });
+            setSTSData(row.original);
             setShowSTSModal(true);
           },
           sx: { cursor: "pointer" },
