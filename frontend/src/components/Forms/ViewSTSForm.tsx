6533ff95084efc42d0ed4ac26a0c7b6ed2ecf5c4
ahmedfahad04 <bsse1204@gmail.com>
2024-03-28 14:01:13 +0600

    added reusable Label for ViewSTS

--- frontend/src/components/Forms/ViewSTSForm.tsx
+++ frontend/src/components/Forms/ViewSTSForm.tsx
@@ -1,4 +1,5 @@
 import { ISTS } from "@/models/STS";
+import Label from "@/ui/Label";
 import { dummyUsers } from "@/utils/DummyData";
 import { InfoIcon } from "lucide-react";
 
@@ -7,32 +8,6 @@ interface ViewSTSFormProps {
 }
 
 const ViewSTSForm: React.FC<ViewSTSFormProps> = ({ stsData }) => {
-  // const { STSName, wardNumber, capacity, latitude, longitude } = stsData;
-
-  //   STSName: "",
-  //   wardNumber: "",
-  //   capacity: "",
-  //   latitude: "",
-  //   longitude: "",
-  // };
-
-  // const [formData, setFormData] = useState({
-  //   STSName: STSName,
-  //   wardNumber: wardNumber,
-  //   capacity: capacity,
-  //   latitude: latitude,
-  //   longitude: longitude,
-  // });
-
-  // const [stsManagers, setSTSManagers] = useState([]);
-
-  // useEffect(() => {
-  //   const managers: IUsers[] = dummyUsers.filter(
-  //     (user) => user.role === "STS Manager"
-  //   );
-  //   setSTSManagers(managers);
-  // }, []);
-
   return (
     <div className="container mx-auto p-4">
       <header className="font-bold text-xl flex flex-row gap-2 items-center">
@@ -45,59 +20,37 @@ const ViewSTSForm: React.FC<ViewSTSFormProps> = ({ stsData }) => {
       </header>
 
       <div className="flex flex-col text-md gap-4 mt-5">
-        <div>
-          <label className="block text-gray-700 font-medium mb-2">
-            STS Name
-          </label>
-          <p className="bg-gray-200 p-2 rounded-md">{stsData?.STSName}</p>
-        </div>
+        <Label title={"STS Name"} value={stsData?.STSName} />
 
         {/* ward & capacity */}
 
         <div className="w-full flex flex-row justify-between items-center gap-5">
           <div className="w-2/4">
-            <label className="block text-gray-700 font-bold mb-2">
-              Ward Number
-            </label>
-            <p className="bg-gray-200 w-full p-2 rounded-md">
-              {stsData?.wardNumber}
-            </p>
+            <Label title={"Ward Number"} value={stsData?.wardNumber} />
           </div>
 
           <div className="w-2/4">
-            <label className="block text-gray-700 font-bold mb-2">
-              STS Capacity (Ton)
-            </label>
-            <p className="bg-gray-200 full p-2 rounded-md">
-              {stsData?.capacity}
-            </p>
+            <Label title={"STS Capacity (Ton)"} value={stsData?.capacity} />
           </div>
         </div>
-
+        {/* lat and long */}
         <div className="w-full flex flex-row justify-between items-center gap-5">
           <div className="w-2/4">
-            <label className="block text-gray-700 font-bold mb-2">
-              Latitude
-            </label>
-            <p className="bg-gray-200 w-full p-2 rounded-md">
-              {stsData?.latitude}
-            </p>
+            <Label title={"Latitude"} value={stsData?.latitude} />
           </div>
 
           <div className="w-2/4">
-            <label className="block text-gray-700 font-bold mb-2">
-              Longitude
-            </label>
-            <p className="bg-gray-200 full p-2 rounded-md">
-              {stsData?.longitude}
-            </p>
+            <Label title={"Longitude"} value={stsData?.longitude} />
           </div>
         </div>
 
+        {/* insert map */}
         <div>
           <label className="block text-gray-700 font-bold mb-2">Map</label>
           {/* <img src={MapImage} alt="Map" className="h-40" /> */}
         </div>
+
+        {/* manager list */}
         <div className="col-span-2">
           <label className="block text-gray-700 font-bold mb-2">
             STS Managers
