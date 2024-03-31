e8857368493a27bdb1fd4ee8d8cf3ded97497aaa
JubaerHosain <jubaerhosain1119@gmail.com>
2024-03-31 20:09:42 +0600

    updated dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -6,13 +6,16 @@ const fetcher = (url: string) => fetch(url, { credentials: "include" }).then((re
 
 const Dashboard = () => {
     const { data: sts } = useSWR<any[]>(`${BASE_URL}/sts`, fetcher);
-    const {data: landfills} = useSWR<any[]>(`${BASE_URL}/landfills`, fetcher);
-    const {data: vehicles} = useSWR<any[]>(`${BASE_URL}/vehicles`, fetcher);
-    
+    const { data: landfills } = useSWR<any[]>(`${BASE_URL}/landfills`, fetcher);
+    const { data: vehicles } = useSWR<any[]>(`${BASE_URL}/vehicles`, fetcher);
+    const { data: trips } = useSWR<any[]>(`${BASE_URL}/trips`, fetcher);
+
+    console.log(trips);
+
     return (
         <Layout>
             <div className="container mx-auto px-4 py-8">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
                     {/* Top Cards */}
                     <div className="bg-white rounded-lg overflow-hidden shadow-md p-6">
                         <h3 className="text-xl font-semibold mb-4">Total Stats</h3>
@@ -33,24 +36,29 @@ const Dashboard = () => {
                     </div>
                     {/* Recent Dumping Status */}
                     <div className="bg-white rounded-lg overflow-hidden shadow-md p-6">
-                        <h3 className="text-xl font-semibold mb-4">Recent Dumping Status</h3>
+                        <h3 className="text-xl font-semibold mb-4">Recent Trip Status</h3>
                         <table className="w-full">
                             <thead>
                                 <tr className="bg-gray-200">
+                                    <th className="py-2 px-4">Vehicle Number</th>
                                     <th className="py-2 px-4">STS Name</th>
-                                    <th className="py-2 px-4">Vehicle Name</th>
+                                    <th className="py-2 px-4">STS Departure Time</th>
                                     <th className="py-2 px-4">Landfill Name</th>
                                     <th className="py-2 px-4">Dumping Time</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td className="py-2 px-4">Center</td>
-                                    <td className="py-2 px-4">Vehicle 1</td>
-                                    <td className="py-2 px-4">Landfill 1</td>
-                                    <td className="py-2 px-4">2024-03-31 12:00 PM</td>
-                                </tr>
-                                {/* Add more rows as needed */}
+                                {trips?.map((trips) => {
+                                    return (
+                                        <tr>
+                                            <td className="py-2 px-4">{trips.vehicle.vehicle_number}</td>
+                                            <td className="py-2 px-4">{trips.sts.sts_name}</td>
+                                            <td className="py-2 px-4">{trips.sts_departure_time}</td>
+                                            <td className="py-2 px-4">{trips.landfill.landfill_name}</td>
+                                            <td className="py-2 px-4">{trips.landfill_dumping_time ?? "Not Dumped yet"}</td>
+                                        </tr>
+                                    );
+                                })}
                             </tbody>
                         </table>
                     </div>
